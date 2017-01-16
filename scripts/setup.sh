9ad75c4705c87169ada7495e67eded83fc6e9890
Jeremy Barbe <capitaine.mousse@gmail.com>
2017-01-16 15:03:01 +0100

    Update to node v7

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ echo "Provisioning virtual machine..."
 
 if [ $(dpkg-query -W -f='${Status}' nodejs 2>/dev/null | grep -c "ok installed") -eq 0 ]
 then
-    bash <(curl -s https://deb.nodesource.com/setup_6.x)
+    bash <(curl -s https://deb.nodesource.com/setup_7.x)
     apt-get upgrade -y
     apt-get install -y build-essential graphicsmagick libstdc++6 nodejs
     npm install -g nodemon forever
